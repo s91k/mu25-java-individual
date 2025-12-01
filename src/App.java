d18d9dcce905d231766c06856d793904b819df46
Simon <s91k@hotmail.com>
2025-12-02 00:06:46 +0100

    add player name and track number of wins, add option to play another round

--- src/App.java
+++ src/App.java
@@ -2,40 +2,72 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        Board b = new Board(3);
-        Player[] players = new Player[] { new Player('o'), new Player('x')};
-        int currentPlayer = 0;
+        Player[] players = new Player[2];
+
         Scanner s  = new Scanner(System.in);
+        System.out.print("Ange namn på spelare 1: ");
+        players[0] = new Player('x', s.nextLine().trim());
 
-        while(true){
-            boolean markPlaced = false;
+        System.out.print("Ange namn på spelare 2: ");
+        players[1] = new Player('o', s.nextLine().trim());
 
-            // Loop until the player makes a valid move
-            while(!markPlaced){
-                System.out.println(b);
-                System.out.println("Spelare " + players[currentPlayer].getMark() + ", vilken ruta (1-9)?");
+        boolean continuePlaying = true;
 
-                try {
-                    int nextMove = Integer.parseUnsignedInt(s.nextLine()) - 1;
-                    markPlaced = b.placeMark(players[currentPlayer].getMark(), nextMove % 3, nextMove / 3);
+        while(continuePlaying){
+            Board b = new Board(3);
+            int currentPlayer = 0;
 
-                    if(!markPlaced){
-                        System.out.println("Ogiltigt drag, rutan måste vara tom.");
+            while(true){
+                boolean markPlaced = false;
+
+                // Loop until the player makes a valid move
+                while(!markPlaced){
+                    System.out.println(b);
+                    System.out.println(players[currentPlayer].getName() + " (" + players[currentPlayer].getMark() + "), vilken ruta (1-9)?");
+
+                    try {
+                        int nextMove = Integer.parseUnsignedInt(s.nextLine()) - 1;
+                        markPlaced = b.placeMark(players[currentPlayer].getMark(), nextMove % 3, nextMove / 3);
+
+                        if(!markPlaced){
+                            System.out.println("Ogiltigt drag, rutan måste vara tom.");
+                        }
+                    } catch(NumberFormatException | IndexOutOfBoundsException _) {
+                        System.out.println("Ogiltigt drag, ange en siffra mellan 1 och 9");
                     }
-                } catch(NumberFormatException | IndexOutOfBoundsException _) {
-                    System.out.println("Ogiltigt drag, ange en siffra mellan 1 och 9");
+                }
+
+                if(b.isWinner(players[currentPlayer].getMark())){
+                    System.out.println(b);
+                    System.out.println(players[currentPlayer].getName() + " (" + players[currentPlayer].getMark() + ") has vunnit!");
+
+                    players[currentPlayer].setNrOfWins(players[currentPlayer].getNrOfWins() + 1);
+                    break;
+                } else if(!b.hasEmptySpots()){
+                    System.out.println(b);
+                    System.out.println("Oavgjort, det går inte att göra några fler drag.");
+                    break;
+                }
+
+                currentPlayer = (currentPlayer + 1) % 2;
+            }
+
+            System.out.println(players[0] + " och " + players[1]);
+
+            // Loop until valid response is given
+            while(true){
+                System.out.println("Vill ni spela igen? (y/n)");
+                String line = s.nextLine();
+
+                if(line.equals("y")){
+                    break;
+                } else if(line.equals("n")){
+                    continuePlaying = false;
+                    break;
                 }
             }
-
-            if(b.isWinner(players[currentPlayer].getMark())){
-                System.out.println("Spelare " + players[currentPlayer].getMark() + " has vunnit!");
-                break;
-            } else if(!b.hasEmptySpots()){
-                System.out.println("Det går inte att göra några fler drag.");
-                break;
-            }
-
-            currentPlayer = (currentPlayer + 1) % 2;
         }
+
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,31 @@
 public class Player {
     private final char mark;
+    private final String name;
+    private int nrOfWins;
 
-    public Player(char mark){
+    public Player(char mark, String name){
         this.mark = mark;
+        this.name = name;
+        this.nrOfWins = 0;
     }
 
-    char getMark(){
+    public char getMark(){
         return mark;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getNrOfWins(){
+        return nrOfWins;
+    }
+
+    public void setNrOfWins(int nrOfWins){
+        this.nrOfWins = nrOfWins;
+    }
+
+    public String toString(){
+        return name + " (" + mark + ") har vunnit " + nrOfWins + " " + (nrOfWins == 1 ? "gång" : "gånger");
     }
 }
